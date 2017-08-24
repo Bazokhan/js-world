32dde31e4626e50fb2a0e127a4860cbb3a8f739e
Bazokhan <albaz2000eg@gmail.com>
2017-08-24 14:23:12 +0200

    Add basic header view and sample data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  javaScript = {
+    topicName: 'JavaScript',
+    articles: [
+      { _id: 'article01', name: 'First Article', url: 'www.google.com', completed: false },
+      { _id: 'article02', name: 'Second Article', url: 'www.yahoo.com', completed: false }
+    ],
+    youtubeVideos: [
+      { _id: 'video01', name: 'First Video', url: 'www.google.com', completed: false },
+      { _id: 'video02', name: 'Second Video', url: 'www.yahoo.com', completed: false }
+    ],
+    books: [
+      { _id: 'book01', name: 'First Book', url: 'www.google.com', completed: false },
+      { _id: 'book02', name: 'Second Book', url: 'www.yahoo.com', completed: false }
+    ]
+  }
+
+  nodeJs = {
+    topicName: 'NodeJs',
+    articles: [
+      { _id: 'article01', name: 'First Article', url: 'www.google.com', completed: false },
+      { _id: 'article02', name: 'Second Article', url: 'www.yahoo.com', completed: false }
+    ],
+    youtubeVideos: [
+      { _id: 'video01', name: 'First Video', url: 'www.google.com', completed: false },
+      { _id: 'video02', name: 'Second Video', url: 'www.yahoo.com', completed: false }
+    ],
+    books: [
+      { _id: 'book01', name: 'First Book', url: 'www.google.com', completed: false },
+      { _id: 'book02', name: 'Second Book', url: 'www.yahoo.com', completed: false }
+    ]
+  }
+
+  sources = [ this.javaScript, this.nodeJs ]
 }
